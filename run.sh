1715981e7093af664c1a607ec6e8707bdb588f92
Sergey Grigoriev <s.a.grigoriev@gmail.com>
2025-01-27 18:22:17 +0200

    Better folder

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
-dataDir="${HOME}/.stable-diffusion"
+dataDir="var/lib/Stable-Diffusion"
+sudo mkdir -p "$dataDir"
+sudo chown :users $dataDir
+sudo chmod g+ws $dataDir
 mkdir -p "$dataDir/workspace"
 mkdir -p "$dataDir/cache"
 echo "Using persistent data folder $dataDir."
